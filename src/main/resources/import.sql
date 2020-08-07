64921da5dffd2dcc31d9ce3e3a44a6d7ca42b943
haubuicodeengine <hau.bui@codeenginestudio.com>
2020-08-07 09:48:04 +0700

    tmp

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,11 @@ INSERT INTO roles (roleid, rolename) VALUES (3, 'Student');
 
 INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10000, 'admin', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'Administrator1', 'admin@codeenginestudio.com', true, 1);
 
+INSERT INTO questiontypes (questiontypeid, questiontypename) VALUES (1, 'Multiple question');
+INSERT INTO questiontypes (questiontypeid, questiontypename) VALUES (2, 'Yes/No question');
+
+-- After finish, delete all rows below
+
 INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10001, 'teacher A', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'teacher1', 'teacher@codeenginestudio.com', true, 2);
 
 INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10002, 'teacher B', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'teacher1', 'teacher@codeenginestudio.com', true, 2);
@@ -28,8 +33,7 @@ INSERT INTO class_students (idrow, classid, studentid) VALUES (300, 100, 10004);
 INSERT INTO class_students (idrow, classid, studentid) VALUES (400, 100, 10005);
 
 
-INSERT INTO questiontypes (questiontypeid, questiontypename) VALUES (1, 'Multiple question');
-INSERT INTO questiontypes (questiontypeid, questiontypename) VALUES (2, 'Yes/No question');
+
 
 INSERT INTO assessments (assessmentid, assessmentname, classid, startdate, expireddate, status) VALUES (100, 'toan hoc', '100', '2020/6/10', '2020/7/20', true);
 
