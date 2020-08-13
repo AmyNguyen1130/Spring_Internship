9b761148b60477ecfcbd42e35203b8e3217d7451
haubuicodeengine <hau.bui@codeenginestudio.com>
2020-08-13 09:51:50 +0700

    Fix can not go to page add question

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,13 +8,13 @@ INSERT INTO questiontypes (questiontypeid, questiontypecode, questiontypename) V
 
 INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10000, 'admin', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'Administrator1', 'admin@codeenginestudio.com', true, 1);
 
-INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10002, 'teacher', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'teacher1', 'teacher@codeenginestudio.com', true, 2);
+INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10002, 'teacher', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'teacher', 'teacher@codeenginestudio.com', true, 2);
 
-INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10006, 'student', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'Administrator1', 'admin11@codeenginestudio.com', true, 3);
+INSERT INTO users (userid, username, password, firstname, email, enabled, roleid) VALUES (10006, 'student', '$2a$11$tUilnpRbUmpBxKWap3oUP..fNsY19D6n/WrdcXc94YnF4yCgXVJty', 'student', 'student@codeenginestudio.com', true, 3);
 
-INSERT INTO classes (classid, classname, status, teacherid) VALUES (100, 'Lop 1', true, 10002);
-INSERT INTO classes (classid, classname, status, teacherid) VALUES (200, 'lop 2', false, 10002);
-INSERT INTO classes (classid, classname,  status, teacherid) VALUES (300, 'lop 3', true, 10003);
+INSERT INTO classes (classid, classname, status, teacherid) VALUES (100, 'Clazz 1', true, 10002);
+INSERT INTO classes (classid, classname, status, teacherid) VALUES (200, 'Clazz 2', false, 10002);
+INSERT INTO classes (classid, classname,  status, teacherid) VALUES (300, 'Clazz 3', true, 10003);
 
 INSERT INTO class_students (idrow, classid, studentid) VALUES (100, 100, 10006);
 
